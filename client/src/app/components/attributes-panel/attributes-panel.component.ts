ee3a288350639efaa130e8aba63439322d1214c0
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2020-09-14 17:47:18 -0400

    Create components to be used in the grid of the editor page

---
+++ client/src/app/components/attributes-panel/attributes-panel.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-attributes-panel',
+    templateUrl: './attributes-panel.component.html',
+    styleUrls: ['./attributes-panel.component.scss'],
+})
+export class AttributesPanelComponent {}
