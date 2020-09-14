5563fdeb0c647d8fe1e40ecba6f2c228b698f22a
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2020-09-14 17:49:24 -0400

    Organize the visual display of components in the editor page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,15 +3,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
+import { AttributesPanelComponent } from './components/attributes-panel/attributes-panel.component';
 import { DrawingComponent } from './components/drawing/drawing.component';
 import { EditorComponent } from './components/editor/editor.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { WorkspaceComponent } from './components/workspace/workspace.component'
 
 @NgModule({
-    declarations: [AppComponent, EditorComponent, SidebarComponent, DrawingComponent, MainPageComponent],
+    declarations: [
+        AppComponent,
+        AttributesPanelComponent,
+        EditorComponent, 
+        SidebarComponent, 
+        DrawingComponent, 
+        MainPageComponent, 
+        WorkspaceComponent
+    ],
     imports: [BrowserModule, HttpClientModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
