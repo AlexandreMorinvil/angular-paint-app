904d33605ea391a43ffda4bcb151cbc38a05a7ec
sowamm <marsousofia@hotmail.fr>
2020-09-16 19:35:16 -0400

    rectangle is finish without 100% tests sofia

--- client/src/app/services/tools/rectangle/rectangle-service.ts
+++ client/src/app/services/tools/rectangle/rectangle-service.ts
@@ -11,16 +11,45 @@ export enum MouseButton {
     Forward = 4,
 }
 
+enum Color {
+    rouge = '#ff0000',
+    noir = '#000000',
+    bleu = '#0000ff',
+    vert = '#008000',
+}
+
+enum LineWidth {
+    onePixel = 1,
+    twoPixel = 2,
+    threePixel = 3,
+    fourPixel = 4,
+    fivePixel = 5,
+}
+
+enum TypeLayout {
+    Full = 'Full',
+    Contour = 'Contour',
+    FullWithContour = 'FullWithContour',
+}
+
 @Injectable({
     providedIn: 'root',
 })
 export class RectangleService extends Tool {
     private pathData: Vec2[];
     private shiftDown: boolean = false;
+    public primaryColor: string;
+    public secondaryColor: string;
+    public lineWidth: number;
+    public typeLayout: string;
 
     constructor(drawingService: DrawingService) {
         super(drawingService);
         this.clearPath();
+        this.primaryColor = Color.vert;
+        this.secondaryColor = Color.noir;
+        this.lineWidth = LineWidth.fourPixel;
+        this.typeLayout = TypeLayout.FullWithContour;
     }
 
     onMouseDown(event: MouseEvent): void {
@@ -74,9 +103,24 @@ export class RectangleService extends Tool {
         if (this.shiftDown) {
             height = width = Math.min(height, width); //draw square on shift pressed
         }
-        ctx.strokeRect(this.mouseDownCoord.x, this.mouseDownCoord.y, width, height);
+        ctx.rect(this.mouseDownCoord.x, this.mouseDownCoord.y, width, height);
+        this.setAttribute(ctx);
         ctx.setLineDash([0]); //pour les pointillés autours
-        ctx.stroke();
+    }
+
+    public setAttribute(ctx: CanvasRenderingContext2D) {
+        if (this.typeLayout == TypeLayout.Full) {
+            ctx.fillStyle = this.primaryColor;
+            ctx.fill();
+        } else if (this.typeLayout == TypeLayout.Contour) {
+            ctx.strokeStyle = this.secondaryColor;
+            ctx.stroke();
+        } else if (this.typeLayout == TypeLayout.FullWithContour) {
+            ctx.fillStyle = this.primaryColor;
+            ctx.strokeStyle = this.secondaryColor;
+            ctx.fill();
+            ctx.stroke();
+        }
     }
 
     private clearPath(): void {
