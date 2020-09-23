e62a9453e8be0973ff4b606fc27519f11a3f9168
sowamm <marsousofia@hotmail.fr>
2020-09-23 19:32:42 -0400

    line fix space problem on jonction sofia

--- client/src/app/services/tools/line/line-service.ts
+++ client/src/app/services/tools/line/line-service.ts
@@ -18,6 +18,7 @@ export class LineService extends Tool {
     private pathData: Vec2[];
     private width: number = 1;
     private mouseUp: boolean;
+    private shiftDown: boolean;
 
     constructor(drawingService: DrawingService) {
         super(drawingService, 'ligne', 'L');
@@ -27,54 +28,33 @@ export class LineService extends Tool {
     onMouseDown(event: MouseEvent): void {
         this.mouseDown = event.button === MouseButton.Left;
         if (this.mouseDown) {
-            //this.clearPath();
+            this.mouseUp = false;
             this.mouseDownCoord = this.getPositionFromMouse(event);
             this.pathData.push(this.mouseDownCoord);
-            //this.drawLine(this.drawingService.baseCtx, this.pathData);
-            //this.clearPath();
-            this.mouseUp = false;
-            //console.log('down and down');
-            //console.log(this.mouseDownCoord);
+            console.log(this.mouseDownCoord);
+            this.drawLine(this.drawingService.baseCtx, this.pathData);
+            this.clearPath();
         }
-        //this.drawLine(this.drawingService.baseCtx, this.pathData);
-        //this.clearPath();
-        //this.mouseDownCoord = this.getPositionFromMouse(event);
-        //this.pathData.push(this.mouseDownCoord);
-        //console.log('down');
-        //console.log(this.mouseDownCoord);
     }
 
     onMouseUp(event: MouseEvent): void {
         if (this.mouseDown) {
             const mousePosition = this.getPositionFromMouse(event);
             this.pathData.push(mousePosition);
-            //this.mouseDownCoord = mousePosition;
         }
-        this.drawLine(this.drawingService.baseCtx, this.pathData);
-        this.clearPath();
-        //console.log('up dans down');
-        //console.log(mousePosition);
-        //this.drawLine(this.drawingService.baseCtx, this.pathData);
-        //this.clearPath();
-        //console.log('up');
-        //console.log(mousePosition);
 
         this.mouseUp = true;
         this.mouseDown = false;
-        //this.clearPath();
     }
 
     onMouseMove(event: MouseEvent): void {
-        if (this.mouseUp) {
+        if (this.mouseUp && !this.mouseDown) {
             const mousePosition = this.getPositionFromMouse(event);
 
             if (this.isInCanvas(mousePosition)) {
                 this.pathData.push(mousePosition);
                 // On dessine sur le canvas de prévisualisation et on l'efface à chaque déplacement de la souris
                 this.drawingService.clearCanvas(this.drawingService.previewCtx);
-                //console.log('move');
-                //console.log(mousePosition);
-
                 this.drawLine(this.drawingService.previewCtx, this.pathData);
             } else {
                 this.drawingService.clearCanvas(this.drawingService.previewCtx);
@@ -83,14 +63,7 @@ export class LineService extends Tool {
         }
     }
 
-    onBackSpaceDown(): void {
-        //this.drawingService.canvas.removeChild()
-        //remove last line
-        //let firstPath = this.pathData[0];
-        let lastPath = this.pathData[this.pathData.length - 1];
-        //console.log(lastPath.x, lastPath.y);
-        this.drawingService.previewCtx.clearRect(lastPath.x, lastPath.y, 1, 0);
-    }
+    onBackSpaceDown(): void {}
 
     onEscapeDown(): void {
         this.mouseUp = false;
@@ -103,6 +76,13 @@ export class LineService extends Tool {
         this.mouseUp = false;
         this.clearPath();
         //console.log('Mouse is double clicked!!!');
+    }
+
+    onShiftDown(): void {
+        this.shiftDown = true;
+    }
+    onShiftUp(): void {
+        this.shiftDown = false;
     }
 
     private isInCanvas(mousePosition: Vec2): boolean {
