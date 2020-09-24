2b00da1ce755f819b3d1d756a433b1a678880066
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2020-09-24 14:34:48 -0400

    Integrate the opacity in the management of colors in the attribute panel

--- client/src/app/components/attributes-panel/attribute-color/attributes-color.component.ts
+++ client/src/app/components/attributes-panel/attribute-color/attributes-color.component.ts
@@ -9,10 +9,14 @@ import { ColorService } from '@app/services/tool-modifier/color/color.service';
 export class AttributeColorComponent {
     private primaryColor: string;
     private secondaryColor: string;
+    private primaryOpacity: number;
+    private secondaryOpacity: number;
 
     constructor(private colorService: ColorService) {
         this.primaryColor = this.colorService.getPrimaryColor();
         this.secondaryColor = this.colorService.getSecondaryColor();
+        this.primaryOpacity = this.colorService.getPrimaryColorOpacity();
+        this.secondaryOpacity = this.colorService.getSecondaryColorOpacity();
     }
 
     get primaryColorDisplayed(): string {
@@ -31,19 +35,41 @@ export class AttributeColorComponent {
         this.secondaryColor = value;
     }
 
+    get primaryOpacityDisplayed(): number {
+        return this.primaryOpacity;
+    }
+
+    set primaryOpacityDisplayed(value: number) {
+        this.primaryOpacity = value;
+    }
+
+    get secondaryOpacityDisplayed(): number {
+        return this.secondaryOpacity;
+    }
+
+    set secondaryOpacityDisplayed(value: number) {
+        this.secondaryOpacity = value;
+    }
+
     assign(): void {
         this.colorService.setPrimaryColor(this.primaryColor);
         this.colorService.setSecondaryColor(this.secondaryColor);
+        this.colorService.setPrimaryColorOpacity(this.primaryOpacity);
+        this.colorService.setSecondaryColorOpacity(this.secondaryOpacity);
     }
 
     revert(): void {
         this.primaryColor = this.colorService.getPrimaryColor();
         this.secondaryColor = this.colorService.getSecondaryColor();
+        this.primaryOpacity = this.colorService.getPrimaryColorOpacity();
+        this.secondaryOpacity = this.colorService.getSecondaryColorOpacity();
     }
 
     needConfirmation(): boolean {
         const hasPrimaryColorChanged = this.primaryColor !== this.colorService.getPrimaryColor();
         const hasSecondaryColorChanged = this.secondaryColor !== this.colorService.getSecondaryColor();
-        return hasPrimaryColorChanged || hasSecondaryColorChanged;
+        const hasPrimaryOpacityChanged = this.primaryOpacity !== this.colorService.getPrimaryColorOpacity();
+        const hasSecondaryOpacityChanged = this.secondaryOpacity !== this.colorService.getSecondaryColorOpacity();
+        return hasPrimaryColorChanged || hasSecondaryColorChanged || hasPrimaryOpacityChanged || hasSecondaryOpacityChanged;
     }
 }
