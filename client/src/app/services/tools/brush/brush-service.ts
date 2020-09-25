f37d801c20c0f050f8c6958e7147012d21ef531d
Christophe Bellemare <christophe.bellemare@polymtl.ca>
2020-09-25 12:16:45 -0400

    fixed the overlapping drawing bug

--- client/src/app/services/tools/brush/brush-service.ts
+++ client/src/app/services/tools/brush/brush-service.ts
@@ -43,6 +43,7 @@ export class BrushService extends Tool {
             const mousePosition = this.getPositionFromMouse(event);
             this.pathData.push(mousePosition);
             this.drawLine(this.drawingService.baseCtx, this.pathData);
+            this.drawingService.clearCanvas(this.drawingService.previewCtx);
         }
         this.mouseDown = false;
         this.clearPath();
