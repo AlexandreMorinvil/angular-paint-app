73575c5e48dc8d577614e818050c36f5ce2c412a
DatPhi_Tran <dat-phi.tran@polymtl.ca>
2020-10-10 16:55:07 -0400

    Added paint service

--- client/src/app/services/tools/paint/paint.service.ts
+++ client/src/app/services/tools/paint/paint.service.ts
@@ -4,20 +4,12 @@ import { Tool } from '@app/classes/tool';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 import { ColorService } from '@app/services/tool-modifier/color/color.service';
 import { ToleranceService } from '@app/services/tool-modifier/tolerance/tolerance.service';
-import { TracingService } from '@app/services/tool-modifier/tracing/tracing.service';
-import { WidthService } from '@app/services/tool-modifier/width/width.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class PaintService extends Tool {
-    constructor(
-        drawingService: DrawingService,
-        private colorService: ColorService,
-        private tracingService: TracingService,
-        public widthService: WidthService,
-        public toleranceService: ToleranceService,
-    ) {
+    constructor(drawingService: DrawingService, private colorService: ColorService, public toleranceService: ToleranceService) {
         super(drawingService, new Description('Paint', 'b', 'paint_icon.png'));
         this.modifiers.push(this.colorService);
         this.modifiers.push(this.toleranceService);
