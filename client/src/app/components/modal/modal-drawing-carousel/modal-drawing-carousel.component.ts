9115547a3e82d14495bdbd882310271399390795
Christophe Bellemare <christophe.bellemare@polymtl.ca>
2020-10-28 15:56:53 -0400

    tags added

--- client/src/app/components/modal/modal-drawing-carousel/modal-drawing-carousel.component.ts
+++ client/src/app/components/modal/modal-drawing-carousel/modal-drawing-carousel.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject } from '@angular/core';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DialogData } from '@app/classes/dialog-data';
+import { MatChipInputEvent } from '@angular/material/chips';
 
 export interface Tag {
     tagName: string;
@@ -12,5 +14,32 @@ export interface Tag {
     styleUrls: ['./modal-drawing-carousel.scss'],
 })
 export class DrawingCarouselComponent {
+    visible: boolean = true;
+    readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+    tags: Tag[] = [];
     constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+    add(event: MatChipInputEvent): void {
+        const input = event.input;
+        const value = event.value;
+
+        // Add the tag
+        if ((value || '').trim()) {
+            this.tags.push({ tagName: value.trim() });
+        }
+
+        // Reset the input value
+        if (input) {
+            input.value = '';
+        }
+    }
+
+    remove(tag: Tag): void {
+        const index = this.tags.indexOf(tag);
+
+        if (index >= 0) {
+            this.tags.splice(index, 1);
+        }
+    }
+
 }
