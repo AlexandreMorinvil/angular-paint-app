bc40d5823d125ef5b1c7be148c5d45d3b30cf25e
Christophe Bellemare <christophe.bellemare@polymtl.ca>
2020-11-02 20:28:48 -0500

    first commit

--- client/src/app/components/modal/modal-drawing-carousel/modal-drawing-carousel.component.ts
+++ client/src/app/components/modal/modal-drawing-carousel/modal-drawing-carousel.component.ts
@@ -6,165 +6,160 @@ import { DialogData } from '@app/classes/dialog-data';
 import { FILE_SERVER_BASE_URL } from '@app/services/api/api-drawing/api-drawing.service';
 import { LoadService } from '@app/services/load/load.service';
 import { RemoteMemoryService } from '@app/services/remote-memory/remote-memory.service.ts';
-import { Tag, TagFilter } from '@app/services/tag-filter/tag-filter.service';
+import { Tag, TagFilterService } from '@app/services/tag-filter/tag-filter.service';
 import { DrawingToDatabase } from '@common/communication/drawingtodatabase';
 
 enum PurposeofClick {
-    Delete,
-    Load,
+  Delete,
+  Load,
 }
 
 @Component({
-    selector: 'app-modal-drawing-carousel',
-    templateUrl: './modal-drawing-carousel.html',
-    styleUrls: ['./modal-drawing-carousel.scss'],
+  selector: 'app-modal-drawing-carousel',
+  templateUrl: './modal-drawing-carousel.html',
+  styleUrls: ['./modal-drawing-carousel.scss'],
 })
 export class DrawingCarouselComponent {
-    private currentDrawings: DrawingToDatabase[] = [];
-    private currentActivesIndexes: number[];
-    private drawingSelectedPurpose: PurposeofClick = PurposeofClick.Load;
-    visible: boolean = true;
-    readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  private currentDrawings: DrawingToDatabase[] = [];
+  private currentActivesIndexes: number[];
+  private drawingSelectedPurpose: PurposeofClick = PurposeofClick.Load;
+  visible: boolean = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
 
-    constructor(
-        public memoryService: RemoteMemoryService,
-        public tagFilterService: TagFilter,
-        public loadService: LoadService,
-        @Inject(MAT_DIALOG_DATA) public data: DialogData,
-    ) {
-        tagFilterService.activeTags = [];
-        for (let i = 0; i < 3; i++) {
-            this.currentDrawings.push({ _id: null, name: 'En chargement', tags: [] });
-        }
+  constructor(
+    public memoryService: RemoteMemoryService,
+    public tagFilterService: TagFilterService,
+    public loadService: LoadService,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+  ) {
+    for (let i = 0; i < 3; i++) {
+      this.currentDrawings.push({ _id: null, name: 'En chargement', tags: [] });
+    }
+    this.memoryService.getAllFromDatabase().then(() => {
+      this.setCurrentImages();
+    });
+  }
+
+  getCurrentDrawings(): DrawingToDatabase[] {
+    return this.currentDrawings;
+  }
+
+  getActiveTags() {
+    return this.tagFilterService.getActiveTags();
+  }
+
+  getDrawingUrl(drawing: DrawingToDatabase) {
+    if (!drawing.name) return 'assets/images/nothing.png';
+    return FILE_SERVER_BASE_URL + drawing._id + '.png';
+  }
+
+  setCurrentImages() {
+    //console.log(this.memoryService.drawingsFromDatabase);
+    this.tagFilterService.filterByTag(this.memoryService.drawingsFromDatabase);
+    this.currentDrawings = [];
+    this.currentActivesIndexes = [0, 1, 2];
+    for (let i of this.currentActivesIndexes) {
+      if (typeof this.tagFilterService.filteredDrawings[i] === 'undefined') {
+        // Insert a placeholder in case there isn't the minimum of 3 data to fill the forms
+        this.currentDrawings.push(new DrawingToDatabase());
+      } else this.currentDrawings.push(this.tagFilterService.filteredDrawings[i]);
+    }
+  }
+
+  addTag(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add the tag
+    if ((value || '').trim()) {
+      this.tagFilterService.addTag({ tagName: value.trim() });
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+
+    // Update the carousel
+    this.tagFilterService.filterByTag(this.memoryService.drawingsFromDatabase);
+    this.setCurrentImages();
+  }
+
+  removeTag(tag: Tag): void {
+    this.tagFilterService.removeTag(tag);
+
+    // Update the carousel
+    this.tagFilterService.filterByTag(this.memoryService.drawingsFromDatabase);
+    this.setCurrentImages();
+  }
+
+  movePrevious() {
+    // If we have below three drawings, dont do anything
+    if (
+      Object.keys(this.currentDrawings[0]).length === 0 ||
+      Object.keys(this.currentDrawings[1]).length === 0 ||
+      Object.keys(this.currentDrawings[2]).length === 0
+    )
+      return;
+
+    for (let i = 0; i < this.currentDrawings.length; i++) {
+      if (this.currentActivesIndexes[i] - 1 < 0) {
+        this.currentDrawings[i] = this.tagFilterService.filteredDrawings[this.tagFilterService.filteredDrawings.length - 1];
+        this.currentActivesIndexes[i] = this.tagFilterService.filteredDrawings.length - 1;
+      } else {
+        this.currentDrawings[i] = this.tagFilterService.filteredDrawings[this.currentActivesIndexes[i] - 1];
+        this.currentActivesIndexes[i] -= 1;
+      }
+    }
+  }
+
+  moveNext() {
+    // If we have below three drawings, dont do anything
+    if (
+      Object.keys(this.currentDrawings[0]).length === 0 ||
+      Object.keys(this.currentDrawings[1]).length === 0 ||
+      Object.keys(this.currentDrawings[2]).length === 0
+    )
+      return;
+
+    for (let i = 0; i < this.currentDrawings.length; i++) {
+      if (this.currentActivesIndexes[i] + 1 > this.tagFilterService.filteredDrawings.length - 1) {
+        this.currentDrawings[i] = this.tagFilterService.filteredDrawings[0];
+        this.currentActivesIndexes[i] = 0;
+      } else {
+        this.currentDrawings[i] = this.tagFilterService.filteredDrawings[this.currentActivesIndexes[i] + 1];
+        this.currentActivesIndexes[i] += 1;
+      }
+    }
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  onShiftDown(event: KeyboardEvent): void {
+    if (event.key === 'ArrowRight') {
+      this.moveNext();
+    } else if (event.key === 'ArrowLeft') {
+      this.movePrevious();
+    }
+  }
+
+  drawingClicked(drawing: DrawingToDatabase) {
+    if (this.drawingSelectedPurpose === PurposeofClick.Load) {
+      this.loadService.loadDraw(this.getDrawingUrl(drawing));
+    } else if (this.drawingSelectedPurpose === PurposeofClick.Delete) {
+      this.memoryService.deleteFromDatabase(drawing._id).then(() => {
         this.memoryService.getAllFromDatabase().then(() => {
-            this.setCurrentImages();
+          this.setCurrentImages();
         });
+      });
+      this.drawingSelectedPurpose = PurposeofClick.Load;
     }
+  }
 
-    getCurrentDrawings(): DrawingToDatabase[] {
-        return this.currentDrawings;
+  deleteDrawingButtonSelected() {
+    if (this.drawingSelectedPurpose === PurposeofClick.Load) {
+      this.drawingSelectedPurpose = PurposeofClick.Delete;
+    } else if (this.drawingSelectedPurpose === PurposeofClick.Delete) {
+      this.drawingSelectedPurpose = PurposeofClick.Load;
     }
-
-    getActiveTags() {
-        return this.tagFilterService.activeTags;
-    }
-
-    getDrawingUrl(drawing: DrawingToDatabase) {
-        if (!drawing.name) return 'assets/images/nothing.png';
-        return FILE_SERVER_BASE_URL + drawing._id + '.png';
-    }
-
-    setCurrentImages() {
-        //console.log(this.memoryService.drawingsFromDatabase);
-        this.tagFilterService.filterByTag(this.memoryService.drawingsFromDatabase);
-        this.currentDrawings = [];
-        this.currentActivesIndexes = [0, 1, 2];
-        for (let i of this.currentActivesIndexes) {
-            if (typeof this.tagFilterService.filteredDrawings[i] === 'undefined') {
-                // Insert a placeholder in case there isn't the minimum of 3 data to fill the forms
-                this.currentDrawings.push(new DrawingToDatabase());
-            } else this.currentDrawings.push(this.tagFilterService.filteredDrawings[i]);
-        }
-    }
-
-    addTag(event: MatChipInputEvent): void {
-        const input = event.input;
-        const value = event.value;
-
-        // Add the tag
-        if ((value || '').trim()) {
-            this.tagFilterService.activeTags.push({ tagName: value.trim() });
-        }
-
-        // Reset the input value
-        if (input) {
-            input.value = '';
-        }
-
-        // Update the carousel
-        this.tagFilterService.filterByTag(this.memoryService.drawingsFromDatabase);
-        this.setCurrentImages();
-    }
-
-    removeTag(tag: Tag): void {
-        const index = this.tagFilterService.activeTags.indexOf(tag);
-
-        if (index >= 0) {
-            this.tagFilterService.activeTags.splice(index, 1);
-        }
-
-        // Update the carousel
-        this.tagFilterService.filterByTag(this.memoryService.drawingsFromDatabase);
-        this.setCurrentImages();
-    }
-
-    movePrevious() {
-        // If we have below three drawings, dont do anything
-        if (
-            Object.keys(this.currentDrawings[0]).length === 0 ||
-            Object.keys(this.currentDrawings[1]).length === 0 ||
-            Object.keys(this.currentDrawings[2]).length === 0
-        )
-            return;
-
-        for (let i = 0; i < this.currentDrawings.length; i++) {
-            if (this.currentActivesIndexes[i] - 1 < 0) {
-                this.currentDrawings[i] = this.tagFilterService.filteredDrawings[this.tagFilterService.filteredDrawings.length - 1];
-                this.currentActivesIndexes[i] = this.tagFilterService.filteredDrawings.length - 1;
-            } else {
-                this.currentDrawings[i] = this.tagFilterService.filteredDrawings[this.currentActivesIndexes[i] - 1];
-                this.currentActivesIndexes[i] -= 1;
-            }
-        }
-    }
-
-    moveNext() {
-        // If we have below three drawings, dont do anything
-        if (
-            Object.keys(this.currentDrawings[0]).length === 0 ||
-            Object.keys(this.currentDrawings[1]).length === 0 ||
-            Object.keys(this.currentDrawings[2]).length === 0
-        )
-            return;
-
-        for (let i = 0; i < this.currentDrawings.length; i++) {
-            if (this.currentActivesIndexes[i] + 1 > this.tagFilterService.filteredDrawings.length - 1) {
-                this.currentDrawings[i] = this.tagFilterService.filteredDrawings[0];
-                this.currentActivesIndexes[i] = 0;
-            } else {
-                this.currentDrawings[i] = this.tagFilterService.filteredDrawings[this.currentActivesIndexes[i] + 1];
-                this.currentActivesIndexes[i] += 1;
-            }
-        }
-    }
-
-    @HostListener('window:keydown', ['$event'])
-    onShiftDown(event: KeyboardEvent): void {
-        if (event.key === 'ArrowRight') {
-            this.moveNext();
-        } else if (event.key === 'ArrowLeft') {
-            this.movePrevious();
-        }
-    }
-
-    drawingClicked(drawing: DrawingToDatabase) {
-        if (this.drawingSelectedPurpose === PurposeofClick.Load) {
-            this.loadService.loadDraw(this.getDrawingUrl(drawing));
-        } else if (this.drawingSelectedPurpose === PurposeofClick.Delete) {
-            this.memoryService.deleteFromDatabase(drawing._id).then(() => {
-                this.memoryService.getAllFromDatabase().then(() => {
-                    this.setCurrentImages();
-                });
-            });
-            this.drawingSelectedPurpose = PurposeofClick.Load;
-        }
-    }
-
-    deleteDrawingButtonSelected() {
-        if (this.drawingSelectedPurpose === PurposeofClick.Load) {
-            this.drawingSelectedPurpose = PurposeofClick.Delete;
-        } else if (this.drawingSelectedPurpose === PurposeofClick.Delete) {
-            this.drawingSelectedPurpose = PurposeofClick.Load;
-        }
-    }
+  }
 }
--- client/src/app/components/modal/modal-drawing-carousel/modal-drawing-carousel.spec.ts
+++ client/src/app/components/modal/modal-drawing-carousel/modal-drawing-carousel.spec.ts
@@ -1,25 +1,46 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogData } from '@app/classes/dialog-data';
+import { LoadService } from '@app/services/load/load.service';
+import { RemoteMemoryService } from '@app/services/remote-memory/remote-memory.service';
+import { TagFilterService } from '@app/services/tag-filter/tag-filter.service';
+import { DrawingCarouselComponent } from './modal-drawing-carousel.component';
 
-// import { DrawingCarouselComponent } from './drawing-carousel.component';
+describe('DrawingCarouselComponent', () => {
+  let component: DrawingCarouselComponent;
+  let fixture: ComponentFixture<DrawingCarouselComponent>;
+  let memoryServiceSpy: jasmine.SpyObj<RemoteMemoryService>;
+  let tagFilterServiceSpy: jasmine.SpyObj<TagFilterService>;
+  let loadServiceSpy: jasmine.SpyObj<LoadService>;
+  let data: DialogData;
 
-// describe('DrawingCarouselComponent', () => {
-//   let component: DrawingCarouselComponent;
-//   let fixture: ComponentFixture<DrawingCarouselComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ DrawingCarouselComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(DrawingCarouselComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    TestBed.configureTestingModule({
+      declarations: [DrawingCarouselComponent],
+      providers: [
+        { provide: RemoteMemoryService, useValue: memoryServiceSpy },
+        { provide: TagFilterService, useValue: tagFilterServiceSpy },
+        { provide: LoadService, useValue: loadServiceSpy },
+        { provide: MAT_DIALOG_DATA, useValue: data },
+      ]
+    })
+      .compileComponents();
+  }));
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DrawingCarouselComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(' should ', () => {
+    component
+    expect(component).toBeTruthy();
+  });
+});
