0502dfeb7c6128faee4de22a8fede6a0d7f255d2
Christophe Bellemare <christophe.bellemare@polymtl.ca>
2020-11-03 09:07:12 -0500

    remote memory started

--- client/src/app/services/remote-memory/remote-memory.service.spec.ts
+++ client/src/app/services/remote-memory/remote-memory.service.spec.ts
@@ -1,16 +1,35 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { ApiDrawingService } from '@app/services/api/api-drawing/api-drawing.service';
+import { DrawingToDatabase } from '@common/communication/drawingtodatabase';
+import { RemoteMemoryService } from './remote-memory.service';
 
-// import { RemoteMemoryService } from './memory.service';
+describe('RemoteMemoryService', () => {
+    let service: RemoteMemoryService;
+    let apiSpy: jasmine.SpyObj<ApiDrawingService>;
+    const dataTest: DrawingToDatabase[] = [
+        { _id: '1', name: 'test1', tags: [] },
+        { _id: '2', name: 'test2', tags: [] },
+    ];
 
-// describe('RemoteMemoryService', () => {
-//   let service: RemoteMemoryService;
+    beforeEach(() => {
+        apiSpy = jasmine.createSpyObj('ApiDrawingService', {
+            getAll: Promise.resolve(true),
+        });
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(RemoteMemoryService);
-//   });
+        TestBed.configureTestingModule({
+            providers: [{ provide: ApiDrawingService, useValue: apiSpy }],
+        });
+        service = TestBed.inject(RemoteMemoryService);
+    });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should get drawings', () => {
+        service.getAllFromDatabase().then();
+        {
+            expect(service.getDrawingsFromDatabase()[0]._id).not.toBe(5);
+        }
+    });
+});
