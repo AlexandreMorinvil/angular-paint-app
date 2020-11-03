3662b2ea55009b6d81b0b861ffac7f3d4a36b384
Christophe Bellemare <christophe.bellemare@polymtl.ca>
2020-11-03 11:31:19 -0500

    test-pipeline

--- client/src/app/services/load/load.service.ts
+++ client/src/app/services/load/load.service.ts
@@ -6,8 +6,8 @@ import { DrawingService } from '@app/services/drawing/drawing.service';
     providedIn: 'root',
 })
 export class LoadService {
-    imageSource: string;
-    constructor(private drawingService: DrawingService, public dialog: MatDialog) {}
+
+  constructor(private drawingService: DrawingService, public dialog: MatDialog) {}
 
     loadDraw(imageSrc: string): void {
         // If the object doesn't contain an image
