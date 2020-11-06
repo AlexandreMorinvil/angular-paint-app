30e9b09114389322d426d3df0bd3dab1713e96bc
DatPhi_Tran <dat-phi.tran@polymtl.ca>
2020-11-05 19:03:32 -0500

    test for load service + Lint

--- client/src/app/services/load/load.service.spec.ts
+++ client/src/app/services/load/load.service.spec.ts
@@ -2,37 +2,51 @@ import { TestBed } from '@angular/core/testing';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { canvasTestHelper } from '@app/classes/canvas-test-helper';
 import { DrawingService } from '@app/services/drawing/drawing.service';
+import { ColorService } from '@app/services/tool-modifier/color/color.service';
+import { TracingService } from '@app/services/tool-modifier/tracing/tracing.service';
 import { LoadService } from './load.service';
-
-class DrawingServiceStub {
-    baseCtx: CanvasRenderingContext2D;
-    previewCtx: CanvasRenderingContext2D;
-}
 
 describe('LoadService', () => {
     let service: LoadService;
-    let drawServiceStub: DrawingServiceStub;
+    let colorService: ColorService;
+    let tracingService: TracingService;
+
     let baseCtxStub: CanvasRenderingContext2D;
     let previewCtxStub: CanvasRenderingContext2D;
     // let closeAllSpy: jasmine.Spy<any>;
     let dataMock: jasmine.SpyObj<MatDialog>;
     let imgStub: HTMLImageElement;
+    let drawServiceSpy: jasmine.SpyObj<DrawingService>;
+    let canvasStub: HTMLCanvasElement;
 
     beforeEach(() => {
+        // tslint:disable:no-any
         baseCtxStub = canvasTestHelper.canvas.getContext('2d') as CanvasRenderingContext2D;
         previewCtxStub = canvasTestHelper.drawCanvas.getContext('2d') as CanvasRenderingContext2D;
-        drawServiceStub = { baseCtx: baseCtxStub, previewCtx: previewCtxStub };
+        canvasStub = canvasTestHelper.canvas;
         imgStub = document.createElement('img');
         dataMock = jasmine.createSpyObj('MatDialog', ['closeAll']);
+        drawServiceSpy = jasmine.createSpyObj('DrawingService', ['clearCanvas']);
 
         TestBed.configureTestingModule({
             imports: [MatDialogModule],
             providers: [
-                { provide: DrawingService, useValue: drawServiceStub },
+                { provide: DrawingService, useValue: drawServiceSpy },
                 { provide: MatDialog, useValue: dataMock },
             ],
         });
+        const canvasWidth = 1000;
+        const canvasHeight = 800;
         service = TestBed.inject(LoadService);
+        tracingService = TestBed.inject(TracingService);
+        colorService = TestBed.inject(ColorService);
+        (service as any).drawingService.baseCtx = baseCtxStub;
+        (service as any).drawingService.previewCtx = previewCtxStub;
+        (service as any).drawingService.canvas = canvasStub;
+        (service as any).drawingService.canvas.width = canvasWidth;
+        (service as any).drawingService.canvas.height = canvasHeight;
+        (service as any).tracingService = tracingService;
+        (service as any).tracingService = colorService;
     });
 
     it('should be created', () => {
@@ -58,14 +72,18 @@ describe('LoadService', () => {
         service.loadDraw('assets/images/nothing.png');
         expect(dataMock.closeAll).not.toHaveBeenCalled();
     });
-    /*
-  it('should load given a white canvas', () => {
-    // tslint:disable:no-string-literal
-    drawServiceStub.baseCtx.fillStyle = '#FFFFFF';
-    drawServiceStub.baseCtx.fillRect(0, 0, drawServiceStub.baseCtx.canvas.width, drawServiceStub.baseCtx.canvas.height);
-    service.loadDraw('/example');
-    service.fillDraw(imgStub);
-    expect(dataMock.closeAll).toHaveBeenCalled();
-  });
-*/
+
+    it('should load given a white canvas', () => {
+        // tslint:disable:no-string-literal
+        (service as any).drawingService.baseCtx.fillStyle = '#FFFFFF';
+        (service as any).drawingService.baseCtx.fillRect(
+            0,
+            0,
+            (service as any).drawingService.baseCtx.canvas.width,
+            (service as any).drawingService.baseCtx.canvas.height,
+        );
+        service.loadDraw('/example');
+        service.fillDraw(imgStub);
+        expect(dataMock.closeAll).toHaveBeenCalled();
+    });
 });
