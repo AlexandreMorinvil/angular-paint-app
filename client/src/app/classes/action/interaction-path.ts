03c7f4e6b36a9eaf2007dc0deb1badd720cd6aaf
sowamm <marsousofia@hotmail.fr>
2020-11-19 12:30:42 -0500

    add function for undo redo aerosol

--- client/src/app/classes/action/interaction-path.ts
+++ client/src/app/classes/action/interaction-path.ts
@@ -2,9 +2,7 @@ import { Vec2 } from '@app/classes/vec2';
 
 export class InteractionPath {
     path: Vec2[];
-    aerosolPath: Vec2[][];
     constructor(vec2: Vec2[]) {
         this.path = vec2;
-        this.aerosolPath.push(vec2);
     }
 }
--- client/src/app/services/tools/aerosol/aerosol.service.ts
+++ client/src/app/services/tools/aerosol/aerosol.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
+import { InteractionPath } from '@app/classes/action/interaction-path';
 import { Description } from '@app/classes/description';
 import { MouseButton } from '@app/classes/mouse';
 import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
+import { DrawingStateTrackerService } from '@app/services/drawing-state-tracker/drawing-state-tracker.service';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 import { ColorService } from '@app/services/tool-modifier/color/color.service';
 import { NumberSprayTransmissionService } from '@app/services/tool-modifier/numberspraytransmission/numberspraytransmission.service';
@@ -17,14 +19,15 @@ export class AerosolService extends Tool {
     private readonly factorTimeIntervalBeetweenSpray: number = 100;
     private pathData: Vec2[];
     private sprayIntervalId: any;
-    //private savedPathData: Vec2[];
+    private savedPathData: Vec2[];
 
     constructor(
         drawingService: DrawingService,
         private colorService: ColorService,
         private sprayService: SprayDiameterService,
         private sprayDropletService: SprayDropletDiameterService,
-        private numberSprayTransmissionService: NumberSprayTransmissionService, //private drawingStateTrackingService: DrawingStateTrackerService,
+        private numberSprayTransmissionService: NumberSprayTransmissionService,
+        private drawingStateTrackingService: DrawingStateTrackerService,
     ) {
         super(drawingService, new Description('aerosol', 'a', 'aerosol_icon.png'));
         this.modifiers.push(this.sprayService);
@@ -59,7 +62,7 @@ export class AerosolService extends Tool {
             this.pathData.push(mousePosition);
         }
         clearInterval(this.sprayIntervalId);
-        //this.drawingStateTrackingService.addAction(this, new InteractionPath(this.savedPathData));
+        this.drawingStateTrackingService.addAction(this, new InteractionPath(this.savedPathData));
         this.mouseDown = false;
         this.clearPath();
     }
@@ -76,8 +79,8 @@ export class AerosolService extends Tool {
         const mouseMoveCoord = path[path.length - 1];
         const xposition = mouseMoveCoord.x;
         const yposition = mouseMoveCoord.y;
-        // const savedData: Vec2 = { x: xposition, y: yposition }; //pour undo redo
-        //this.savedPathData.push(savedData); //pour undo redo
+        const savedData: Vec2 = { x: xposition, y: yposition }; //pour undo redo
+        this.savedPathData.push(savedData); //pour undo redo
         if (this.isInCanvas(mouseMoveCoord)) {
             const numberSprayTransmission = this.numberSprayTransmissionService.getNumberSprayTransmission() / this.factorTimeIntervalBeetweenSpray;
             for (let i = 0; i < numberSprayTransmission; i++) {
@@ -106,19 +109,21 @@ export class AerosolService extends Tool {
         ctx.globalAlpha = this.colorService.getSecondaryColorOpacity();
     }
 
-    // private redoSprayPaint(ctx: CanvasRenderingContext2D, path: Vec2[]): void {
-    //     for (let i = 0; i < path.length; i++) {
-    //         ctx.arc(path[i].x, path[i].y, 1, 0, 2 * Math.PI, false);
-    //         ctx.fill();
-    //     }
-    // }
+    private redoSprayPaint(ctx: CanvasRenderingContext2D, path: Vec2[]): void {
+        this.setAttribute(ctx);
+        for (let i = 0; i < path.length; i++) {
+            ctx.beginPath();
+            ctx.arc(path[i].x, path[i].y, 1, 0, 2 * Math.PI, false);
+            ctx.fill();
+        }
+    }
 
     private clearPath(): void {
         this.pathData = [];
-        //this.savedPathData = [];
+        this.savedPathData = [];
     }
 
-    // execute(interaction: InteractionPath): void {
-    //     this.redoSprayPaint(this.drawingService.baseCtx, interaction.path);
-    // }
+    execute(interaction: InteractionPath): void {
+        this.redoSprayPaint(this.drawingService.baseCtx, interaction.path);
+    }
 }
