7b8c05eaaf0706d50314d6fad1e55da08aae81f0
Aime <aime.irabahaye@polymtl.ca>
2020-11-19 18:31:55 -0500

    lint fix

--- client/src/app/services/auto-save/auto-save.service.spec.ts
+++ client/src/app/services/auto-save/auto-save.service.spec.ts
@@ -1,16 +1,14 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async, inject } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 import { AutoSaveService } from './auto-save.service';
 
 describe('Service: AutoSave', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [AutoSaveService]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [AutoSaveService],
+        });
     });
-  });
 
-  it('should ...', inject([AutoSaveService], (service: AutoSaveService) => {
-    expect(service).toBeTruthy();
-  }));
+    it('should ...', inject([AutoSaveService], (service: AutoSaveService) => {
+        expect(service).toBeTruthy();
+    }));
 });
