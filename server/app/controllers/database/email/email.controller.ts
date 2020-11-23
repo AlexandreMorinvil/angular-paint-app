14a1077cbfb18ceb4f0f07a8310ebe403fda78ef
Aime <aime.irabahaye@polymtl.ca>
2020-11-22 21:57:10 -0500

    envoi par courriel fonctionnel avec valeurs env hardcoded

--- server/app/controllers/database/email/email.controller.ts
+++ server/app/controllers/database/email/email.controller.ts
@@ -1,7 +1,7 @@
 import { EmailService } from '@app/services/email/email.service';
 import { TYPES } from '@app/types';
 import { NextFunction, Request, Response, Router } from 'express';
-import { StatusCodes } from 'http-status-codes';
+import * as fs from 'fs';
 import { inject, injectable } from 'inversify';
 
 @injectable()
@@ -13,16 +13,22 @@ export class EmailController {
         this.configureRouter();
     }
 
-    private configureRouter() {
+    private configureRouter(): void {
         this.router = Router();
 
         this.router.post(this.ROUTING_POST, async (req: Request, res: Response, next: NextFunction) => {
-            this.emailService
-                .sendEmail(req.body)
-                .then(() => {})
-                .catch((error: Error) => {
-                    res.status(StatusCodes.NOT_FOUND).send(error.message);
-                });
+            const email = req.body.emailAdress;
+            const data = req.body.imageSrc.split(';base64,');
+            const base64Image = data.pop();
+            const ext = data[0].split('/').pop();
+
+            fs.writeFile('temp.' + ext, base64Image, { encoding: 'base64' }, async () => {
+                console.log('File created');
+                const code = await this.emailService.sendEmail(email, ext);
+                res.status(code).send({ returnCode: code });
+            });
+
+            return;
         });
     }
 }
--- server/app/services/email/email.service.ts
+++ server/app/services/email/email.service.ts
@@ -1,10 +1,46 @@
-import { DrawingToEmail } from '@common/communication/drawing-to-email';
+import * as FormData from 'form-data';
+import * as fs from 'fs';
+import { request } from 'http';
 import { injectable } from 'inversify';
 import 'reflect-metadata';
 
 @injectable()
 export class EmailService {
-    async sendEmail(drawing: DrawingToEmail): Promise<void> {
-        // a continuer
+    async sendEmail(email: string, ext: string): Promise<number> {
+        const form = new FormData();
+        form.append('to', email);
+        const file = fs.createReadStream('temp.' + ext);
+        form.append('payload', file);
+        const req = request(
+            {
+                host: 'log2990.step.polymtl.ca',
+                method: 'POST',
+                path: '/email',
+                headers: {
+                    'content-type': 'multipart/form-data; boundary=' + form.getBoundary(),
+                    'X-Team-Key': '1e2ccc05-3dc7-4da8-8faf-5a2f7b703153',
+                },
+            },
+            (res) => {
+                let content = '';
+                res.on('data', (chunk) => {
+                    content += chunk;
+                });
+
+                res.on('end', () => {
+                    console.log(content);
+                });
+            },
+        );
+
+        form.pipe(req);
+
+        let code: number;
+        return new Promise<number>((resolve) => {
+            req.on('response', (res) => {
+                code = res.statusCode as number;
+                resolve(code);
+            });
+        });
     }
 }
