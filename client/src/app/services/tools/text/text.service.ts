c004793644de31f0cb7767004f4b57c20e4f0496
Christophe Bellemare <christophe.bellemare@polymtl.ca>
2020-11-26 18:05:20 -0500

    refactored

--- client/src/app/services/tools/text/text.service.ts
+++ client/src/app/services/tools/text/text.service.ts
@@ -12,7 +12,7 @@ export class TextService extends Tool {
     private numberOfLines: number = 1;
     private textPosition: Vec2;
     private editingOn: boolean = false;
-    private cursorPosition: Vec2;
+    private cursorPosition: Vec2 = { x: 0, y: 0 };
     private spaceBetweenLines: number = 10;
     constructor(public drawingService: DrawingService) {
         super(drawingService, new Description('texte', 't', 'text_icon.png'));
@@ -22,7 +22,6 @@ export class TextService extends Tool {
             this.editingOn = true;
             this.drawingService.shortcutEnable = false;
             this.textPosition = this.getPositionFromMouse(event);
-            this.cursorPosition = this.getPositionFromMouse(event);
             this.showCursor();
         } else {
             this.editingOn = false;
@@ -40,9 +39,11 @@ export class TextService extends Tool {
                 this.onEnterDown();
             } else if (event.key === 'Backspace') {
                 this.onBackspaceDown();
+            } else if (event.key === 'ArrowLeft' || event.key === 'ArrowRight' || event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+                this.onArrowDown(event);
             } else {
                 this.text[this.numberOfLines - 1] = this.text[this.numberOfLines - 1].concat(event.key);
-                this.cursorPosition.x += this.drawingService.previewCtx.measureText(event.key).width;
+                this.cursorPosition.x += 1;
             }
             const adjustment = this.findTextPositionAdjustment();
             this.drawingService.clearCanvas(this.drawingService.previewCtx);
@@ -68,32 +69,45 @@ export class TextService extends Tool {
         const cursorTopAdjustment = 10;
         const cursorBottomAdjustment = 5;
         this.drawingService.previewCtx.strokeStyle = '#000000';
+        const textLength: number = this.drawingService.baseCtx.measureText(this.text[this.numberOfLines - 1]).width;
         this.drawingService.previewCtx.beginPath();
-        this.drawingService.previewCtx.lineTo(this.cursorPosition.x - adjustement, this.cursorPosition.y - cursorTopAdjustment);
-        this.drawingService.previewCtx.lineTo(this.cursorPosition.x - adjustement, this.cursorPosition.y + cursorBottomAdjustment);
+        this.drawingService.previewCtx.lineTo(
+            this.textPosition.x + textLength - adjustement,
+            this.textPosition.y + this.cursorPosition.y * this.spaceBetweenLines - cursorTopAdjustment,
+        );
+        this.drawingService.previewCtx.lineTo(
+            this.textPosition.x + textLength - adjustement,
+            this.textPosition.y + this.cursorPosition.y * this.spaceBetweenLines + cursorBottomAdjustment,
+        );
         this.drawingService.previewCtx.stroke();
     }
 
     private onEnterDown(): void {
         this.numberOfLines++;
         this.text[this.numberOfLines - 1] = '';
-        this.cursorPosition.x = this.textPosition.x;
-        this.cursorPosition.y += this.spaceBetweenLines;
+        this.cursorPosition.x = 0;
+        this.cursorPosition.y += 1;
     }
     private onBackspaceDown(): void {
         if (this.text[this.numberOfLines - 1] === '' && this.numberOfLines === 1) {
             // dont do anything
         } else if (this.text[this.numberOfLines - 1] === '') {
             this.numberOfLines--;
-            this.cursorPosition.x = this.textPosition.x + this.drawingService.baseCtx.measureText(this.text[this.numberOfLines - 1]).width;
-            this.cursorPosition.y -= this.spaceBetweenLines;
+            this.cursorPosition.x = this.text[this.numberOfLines - 1].length;
+            this.cursorPosition.y = this.numberOfLines - 1;
         } else {
-            const lastCharacterWidth = this.drawingService.baseCtx.measureText(
-                this.text[this.numberOfLines - 1].charAt(this.text[this.numberOfLines - 1].length - 1),
-            ).width;
             const lastIndex = -1;
             this.text[this.numberOfLines - 1] = this.text[this.numberOfLines - 1].slice(0, lastIndex);
-            this.cursorPosition.x -= lastCharacterWidth;
+            this.cursorPosition.x = this.text[this.numberOfLines - 1].length;
+        }
+    }
+
+    private onArrowDown(event: KeyboardEvent): void {
+        if (event.key === 'ArrowUp') {
+            if (this.numberOfLines === 1) {
+                //dont do anything
+            } else {
+            }
         }
     }
 }
