7a9effed33a537dd545843bb2ca0c591eeb80f33
Christophe Bellemare <christophe.bellemare@polymtl.ca>
2020-11-27 13:49:47 -0500

    style service created

---
+++ client/src/app/services/tool-modifier/style/style.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+import { ToolModifier } from '@app/classes/tool-modifier';
+
+export enum TextAlignment {
+    left = 'gauche',
+    center = 'centre',
+    right = 'droit',
+}
+
+export enum TextFont {
+    arial = 'Arial',
+    verdana = 'Verdana',
+    timesNewRoman = 'Times New Roman',
+    courierNew = 'Courier New',
+    serif = 'serif',
+}
+
+@Injectable({
+    providedIn: 'root',
+})
+export class StyleService extends ToolModifier {
+    readonly DEFAULT_ALIGNEMENT: string = TextAlignment.left;
+    private listAlignments: string[];
+    private alignment: string = TextAlignment.left;
+    readonly DEFAULT_FONT: string = TextFont.arial;
+    private listFonts: string[];
+    private font: string = TextFont.arial;
+    readonly DEFAULT_HAS_BOLD: boolean = false;
+    readonly DEFAULT_HAS_ITALIC: boolean = false;
+    private hasBold: boolean = this.DEFAULT_HAS_BOLD;
+    private hasItalic: boolean = this.DEFAULT_HAS_ITALIC;
+
+    constructor() {
+        super();
+        this.listAlignments = Object.values(TextAlignment);
+        this.listFonts = Object.values(TextFont);
+    }
+
+    getListAlignments(): string[] {
+        return this.listAlignments;
+    }
+
+    getAlignment(): string {
+        return this.alignment;
+    }
+
+    setAlignment(input: string): void {
+        if (this.listAlignments.includes(input)) this.alignment = input;
+    }
+
+    getListFonts(): string[] {
+        return this.listFonts;
+    }
+
+    getFont(): string {
+        return this.font;
+    }
+
+    setFont(input: string): void {
+        if (this.listFonts.includes(input)) this.font = input;
+    }
+    setHasBold(input: boolean): void {
+        this.hasBold = input;
+    }
+
+    getHasBold(): boolean {
+        return this.hasBold;
+    }
+
+    getHasItalic(): boolean {
+        return this.hasItalic;
+    }
+
+    setHasItalic(input: boolean): void {
+        this.hasItalic = input;
+    }
+}
