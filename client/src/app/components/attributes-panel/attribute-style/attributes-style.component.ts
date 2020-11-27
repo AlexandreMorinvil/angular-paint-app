2492d192b8ce3244aa30108e7bdbd560ef31a98d
Christophe Bellemare <christophe.bellemare@polymtl.ca>
2020-11-27 14:56:19 -0500

    added font and alignment

--- client/src/app/components/attributes-panel/attribute-style/attributes-style.component.ts
+++ client/src/app/components/attributes-panel/attribute-style/attributes-style.component.ts
@@ -9,23 +9,42 @@ import { StyleService } from '@app/services/tool-modifier/style/style.service';
 export class AttributeStyleComponent {
     hasBold: boolean;
     hasItalic: boolean;
+    alignment: string;
+    font: string;
 
     constructor(private styleService: StyleService) {
         this.hasBold = this.styleService.getHasBold();
         this.hasItalic = this.styleService.getHasItalic();
+        this.alignment = this.styleService.getAlignment();
+        this.font = this.styleService.getFont();
     }
 
+    getListAlignments() {
+        return this.styleService.getListAlignments();
+    }
+    getListFonts() {
+        return this.styleService.getListFonts();
+    }
     assign(): void {
         this.styleService.setHasBold(this.hasBold);
         this.styleService.setHasItalic(this.hasItalic);
+        this.styleService.setAlignment(this.alignment);
+        this.styleService.setFont(this.font);
     }
 
     revert(): void {
         this.hasBold = this.styleService.getHasBold();
         this.hasItalic = this.styleService.getHasItalic();
+        this.alignment = this.styleService.getAlignment();
+        this.font = this.styleService.getFont();
     }
 
     needConfirmation(): boolean {
-        return this.hasItalic !== this.styleService.getHasItalic() || this.hasBold !== this.styleService.getHasBold();
+        return (
+            this.hasItalic !== this.styleService.getHasItalic() ||
+            this.hasBold !== this.styleService.getHasBold() ||
+            this.alignment !== this.styleService.getAlignment() ||
+            this.font !== this.styleService.getFont()
+        );
     }
 }
