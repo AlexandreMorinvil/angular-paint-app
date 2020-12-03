daaa8c0113ced879eecdc8ee516f4956e5e1d512
Aime <aime.irabahaye@polymtl.ca>
2020-12-03 17:46:19 -0500

    refactor du code + utilisation variable d'environnement pour les clee d'API

--- server/app/controllers/database/email/email.controller.ts
+++ server/app/controllers/database/email/email.controller.ts
@@ -1,7 +1,7 @@
 import { EmailService } from '@app/services/email/email.service';
 import { TYPES } from '@app/types';
 import { NextFunction, Request, Response, Router } from 'express';
-import * as fs from 'fs';
+import { StatusCodes } from 'http-status-codes';
 import { inject, injectable } from 'inversify';
 
 @injectable()
@@ -15,20 +15,15 @@ export class EmailController {
 
     private configureRouter(): void {
         this.router = Router();
-
         this.router.post(this.ROUTING_POST, async (req: Request, res: Response, next: NextFunction) => {
-            const email = req.body.emailAdress;
-            const data = req.body.imageSrc.split(';base64,');
-            const base64Image = data.pop();
-            const ext = data[0].split('/').pop();
-
-            fs.writeFile('temp.' + ext, base64Image, { encoding: 'base64' }, async () => {
-                console.log('File created');
-                const code = await this.emailService.sendEmail(email, ext);
-                res.status(code).send({ returnCode: code });
-            });
-
-            return;
+            this.emailService
+                .processRequest(req.body)
+                .then(() => {
+                    res.sendStatus(StatusCodes.OK);
+                })
+                .catch((error: Error) => {
+                    res.status(StatusCodes.NOT_FOUND).send(error.message);
+                });
         });
     }
 }
--- server/app/services/email/email.service.ts
+++ server/app/services/email/email.service.ts
@@ -6,32 +6,34 @@ import 'reflect-metadata';
 
 @injectable()
 export class EmailService {
-    async sendEmail(email: string, ext: string): Promise<number> {
+    processRequest(requestBody: any): Promise<number> {
+        const dotenv = require('dotenv');
+        dotenv.config();
+
+        const email = requestBody.emailAdress;
+        const data = requestBody.imageSrc.split(';base64,');
+        const base64Image = data.pop();
+        const ext = data[0].split('/').pop();
+
+        fs.writeFileSync('temp.' + ext, base64Image, { encoding: 'base64' });
+
+        return this.sendEmail(email, ext);
+    }
+
+    sendEmail(email: string, ext: string): Promise<number> {
         const form = new FormData();
         form.append('to', email);
         const file = fs.createReadStream('temp.' + ext);
         form.append('payload', file);
-        const req = request(
-            {
-                host: 'log2990.step.polymtl.ca',
-                method: 'POST',
-                path: '/email',
-                headers: {
-                    'content-type': 'multipart/form-data; boundary=' + form.getBoundary(),
-                    'X-Team-Key': '1e2ccc05-3dc7-4da8-8faf-5a2f7b703153',
-                },
+        const req = request({
+            host: 'log2990.step.polymtl.ca',
+            method: 'POST',
+            path: '/email',
+            headers: {
+                'content-type': 'multipart/form-data; boundary=' + form.getBoundary(),
+                'X-Team-Key': process.env.API_KEY,
             },
-            (res) => {
-                let content = '';
-                res.on('data', (chunk) => {
-                    content += chunk;
-                });
-
-                res.on('end', () => {
-                    console.log(content);
-                });
-            },
-        );
+        });
 
         form.pipe(req);
 
