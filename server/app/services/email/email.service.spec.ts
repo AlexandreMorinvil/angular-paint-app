5d6b7c66e6abf2dbbbe049bc75a57d076c60f8ab
sowamm <marsousofia@hotmail.fr>
2020-12-07 15:30:45 -0500

    add test on feature/email

--- server/app/services/email/email.service.spec.ts
+++ server/app/services/email/email.service.spec.ts
@@ -30,4 +30,21 @@ describe('Email Service', () => {
         emailService.processRequest(DRAWING_TO_EMAIL);
         sinon.assert.called(SPY_ON_WRITE_FILE);
     });
+
+    it('Process request should call sendEmail', () => {
+        const DRAWING_TO_EMAIL = new DrawingToEmail('a@hotmail.com', 'alex', 'png', 'IMAGESOURCE');
+        const SPY_ON_SEND_EMAIL = sinon.spy(emailService, 'sendEmail');
+        emailService.processRequest(DRAWING_TO_EMAIL);
+        sinon.assert.called(SPY_ON_SEND_EMAIL);
+    });
+
+    it('sendEmail should call readStream ', () => {
+        const DRAWING_TO_EMAIL = new DrawingToEmail('a@hotmail.com', 'alex', 'png', 'IMAGESOURCE');
+        const SPY_ON_READ_STREAM = sinon.spy(fs, 'createReadStream');
+        try {
+            emailService.sendEmail(DRAWING_TO_EMAIL.emailAdress, DRAWING_TO_EMAIL.format);
+        } catch (error) {
+            sinon.assert.called(SPY_ON_READ_STREAM);
+        }
+    });
 });
