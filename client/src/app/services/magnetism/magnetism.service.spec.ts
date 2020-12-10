f1db609deecec1f93c0d6755c7225d4812ca4bb9
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2020-12-09 19:59:41 -0500

    Complete the tests for the magnetism service

--- client/src/app/services/magnetism/magnetism.service.spec.ts
+++ client/src/app/services/magnetism/magnetism.service.spec.ts
@@ -1,79 +1,104 @@
-// import { TestBed } from '@angular/core/testing';
-// import { canvasTestHelper } from '@app/classes/canvas-test-helper';
-// import { SpacingService } from '@app/services/tool-modifier/spacing/spacing.service';
-// import { MagnetismService } from './magnetism.service';
+import { TestBed } from '@angular/core/testing';
+import { Vec2 } from '@app/classes/vec2';
+import { SpacingService } from '@app/services/tool-modifier/spacing/spacing.service';
+import { MagnetismService } from './magnetism.service';
 
-// // tslint:disable:no-any
-// describe('MagnetismService', () => {
-//     let service: MagnetismService;
-//     let spacingService: SpacingService;
-//     let resetGridSpy: jasmine.Spy<any>;
+// tslint:disable:no-any
+fdescribe('MagnetismService', () => {
+    let service: MagnetismService;
+    let spacingService: SpacingService;
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({});
-//         service = TestBed.inject(MagnetismService);
-//         spacingService = TestBed.inject(SpacingService);
-//         service.gridCanvas = canvasTestHelper.canvas;
-//         service.gridCtx = canvasTestHelper.canvas.getContext('2d') as CanvasRenderingContext2D;
-//         resetGridSpy = spyOn<any>(service, 'resetGrid').and.callThrough();
-//     });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(MagnetismService);
+        spacingService = TestBed.inject(SpacingService);
+    });
 
-//     it('should be created', () => {
-//         expect(service).toBeTruthy();
-//     });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 
-//     it('should draw the grid if the grid is activated', () => {
-//         const drawGridSpy = spyOn<any>(service, 'drawGrid').and.callThrough();
-//         const setAttributesGridSpy = spyOn<any>(service, 'setAttribtes').and.callThrough();
-//         (service as any).isGridOn = true;
-//         service.resetGrid();
-//         expect(drawGridSpy).toHaveBeenCalled();
-//         expect(setAttributesGridSpy).toHaveBeenCalled();
-//     });
+    it('should turn on the magnetism', () => {
+        service.isActivated = false;
+        service.toogleMagnetism();
+        expect(service.isActivated).toBe(true);
+    });
 
-//     it('should not draw the grid if the grid is deactivated', () => {
-//         const drawGridSpy = spyOn<any>(service, 'drawGrid').and.callThrough();
-//         const setAttributesGridSpy = spyOn<any>(service, 'setAttribtes').and.callThrough();
-//         (service as any).isGridOn = false;
-//         service.resetGrid();
-//         expect(drawGridSpy).not.toHaveBeenCalled();
-//         expect(setAttributesGridSpy).not.toHaveBeenCalled();
-//     });
+    it('should give the possition that is closest relative to the upper left corner', () => {
+        (spacingService as any).spacing = 50;
+        const POSITION: Vec2 = { x: 10, y: 10 };
+        const WIDTH: number = 10;
+        const HEIGHT: number = 10;
 
-//     it('should turn the grid from on to off', () => {
-//         (service as any).isGridOn = true;
-//         service.toogleGrid();
-//         expect(service.isGridActivated()).toBe(false);
-//         expect(resetGridSpy).toHaveBeenCalled();
-//     });
+        const CLOSEST_POSITION: Vec2 = service.getAdjustedPositionFromCenter(POSITION, WIDTH, HEIGHT);
 
-//     it('should turn the grid from off to on', () => {
-//         (service as any).isGridOn = false;
-//         service.toogleGrid();
-//         expect(service.isGridActivated()).toBe(true);
-//         expect(resetGridSpy).toHaveBeenCalled();
-//     });
+        expect(CLOSEST_POSITION.x).toBe(0);
+        expect(CLOSEST_POSITION.y).toBe(0);
+    });
 
-//     it(' should increment the spacing of the grid', () => {
-//         const spacingServiceSpy = spyOn<any>(spacingService, 'stepUp').and.callThrough();
-//         service.incrementSpacing();
-//         expect(spacingServiceSpy).toHaveBeenCalled();
-//         expect(resetGridSpy).toHaveBeenCalled();
-//     });
+    it('should determine the left size jump distance to the clossest horizontal grid line', () => {
+        (spacingService as any).spacing = 50;
+        const X_POSITION: number = 20;
+        const WIDTH: number = 10;
 
-//     it(' should decrement the spacing of the grid', () => {
-//         const spacingServiceSpy = spyOn<any>(spacingService, 'stepDown').and.callThrough();
-//         service.decrementSpacing();
-//         expect(spacingServiceSpy).toHaveBeenCalled();
-//         expect(resetGridSpy).toHaveBeenCalled();
-//     });
+        const CLOSEST_POSITION: number = service.getGridHorizontalJumpDistance(X_POSITION, WIDTH, false);
+        const EXPECT_POSITION: number = 25;
 
-//     it('should call resetDrawingWithWarning and ask before delete with answer true', () => {
-//         const newWidth = 222;
-//         const newHeight = 333;
-//         service.resize(newWidth, newHeight);
-//         expect(service.gridCanvas.width).toBe(newWidth);
-//         expect(service.gridCanvas.height).toBe(newHeight);
-//         expect(resetGridSpy).toHaveBeenCalled();
-//     });
-// });
+        expect(CLOSEST_POSITION).toBe(EXPECT_POSITION);
+    });
+
+    it('should determine the right size jump distance to the clossest horizontal grid line', () => {
+        (spacingService as any).spacing = 50;
+        const X_POSITION: number = 20;
+        const WIDTH: number = 10;
+
+        const CLOSEST_POSITION: number = service.getGridHorizontalJumpDistance(X_POSITION, WIDTH, true);
+        const EXPECT_POSITION: number = 25;
+
+        expect(CLOSEST_POSITION).toBe(EXPECT_POSITION);
+    });
+
+    it('should determine the up size jump distance to the clossest horizontal grid line', () => {
+        (spacingService as any).spacing = 50;
+        const Y_POSITION: number = 20;
+        const HEIGHT: number = 10;
+
+        const CLOSEST_POSITION: number = service.getVerticalJumpDistance(Y_POSITION, HEIGHT, false);
+        const EXPECT_POSITION: number = 25;
+
+        expect(CLOSEST_POSITION).toBe(EXPECT_POSITION);
+    });
+
+    it('should determine the bottom size jump distance to the clossest horizontal grid line', () => {
+        (spacingService as any).spacing = 50;
+        const Y_POSITION: number = 20;
+        const HEIGHT: number = 10;
+
+        const CLOSEST_POSITION: number = service.getVerticalJumpDistance(Y_POSITION, HEIGHT, true);
+        const EXPECT_POSITION: number = 25;
+
+        expect(CLOSEST_POSITION).toBe(EXPECT_POSITION);
+    });
+
+    it('should jump a fill grid spacing horizontally if it is already on a grid line', () => {
+        (spacingService as any).spacing = 50;
+        const X_POSITION: number = 45;
+        const WIDTH: number = 10;
+
+        const CLOSEST_POSITION: number = service.getGridHorizontalJumpDistance(X_POSITION, WIDTH, true);
+        const EXPECT_POSITION: number = 50;
+
+        expect(CLOSEST_POSITION).toBe(EXPECT_POSITION);
+    });
+
+    it('should jump a fill grid spacing horizontally if it is already on a grid line', () => {
+        (spacingService as any).spacing = 50;
+        const Y_POSITION: number = 45;
+        const HEIGHT: number = 10;
+
+        const CLOSEST_POSITION: number = service.getVerticalJumpDistance(Y_POSITION, HEIGHT, false);
+        const EXPECT_POSITION: number = 50;
+
+        expect(CLOSEST_POSITION).toBe(EXPECT_POSITION);
+    });
+});
